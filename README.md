f523751aea61d52563d62520a362c67031bb690a
Janek Brandt and Liam Stabeno <janek@>
2017-03-01 09:53:06 -0800

    add method to pass turn, adding tempscore to score. Modify object to include myTurn boolean, this gets changed when player.pass is called

--- README.md
+++ README.md
@@ -14,8 +14,8 @@ _This project will allow a user to play a game of pig dice against another user.
 | -------------------------- | -----------------:| -----------------:|
 | initialize a player object with 2 variables: tempScore and score both = 0 | player1 = new Player() | { player: "player1", score: 0, tempScore: 0 } |
 | player object has roll method that will generate random number between 1-6. If the number is greater than or equal to 2 it gets added to tempScore and allows the player to choose to roll again. | player1.roll() | { player: "player1", score: 0, tempScore: 4 } |
-| player object has stop method that will end the players turn, it will add the tempScore variable to the score variable and set tempScore back to 0 | player1.stop() | { player: "player1", score: 4, tempScore: 0 } |
-| if roll is equal to 1, tempScore variable becomes 0 and the stop method is run | player1.roll() === 1 | { player: "player1", score: 4, tempScore: 0 } |
+| player object has pass method that will end the players turn, it will add the tempScore variable to the score variable and set tempScore back to 0 | player1.pass() | { player: "player1", score: 4, tempScore: 0 } |
+| if roll is equal to 1, tempScore variable becomes 0 and the pass method is run | player1.roll() === 1 | { player: "player1", score: 4, tempScore: 0 } |
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,58 @@
 // Back-End
-function Player() {
+function Player(turn) {
   this.score = 0;
   this.tempScore = 0;
+  this.myTurn = turn;
 };
 
 Player.prototype.roll = function() {
-  var roll = Math.ceil(Math.random() * 6);
-  console.log(roll);
-  if (roll > 1) {
-    this.tempScore += roll;
-  } else {
-    // rolled 1
+  if (this.myTurn) {
+    var roll = Math.ceil(Math.random() * 6);
+    console.log(roll);
+    if (roll > 1) {
+      this.tempScore += roll;
+    } else {
+      this.tempScore = 0
+      return this.pass();
+    };
   };
+};
+
+Player.prototype.pass = function() {
+  console.log("pass called");
+  this.score += this.tempScore;
+  this.tempScore = 0;
+  this.myTurn = false;
+  return true;
 };
 
 // Front-End
 $(function() {
   $("#buttonStart").click(function() {
-    var player1 = new Player();
-    console.log(player1);
+    var player1 = new Player(true);
+    var player2 = new Player(false);
+    console.log("player1 ", player1);
+    console.log("player2 ", player2);
 
     $("#buttonRoll").click(function() {
-      player1.roll();
-      console.log(player1);
+      if (player1.myTurn) {
+        player2.myTurn = player1.roll();
+      } else if (player2.myTurn) {
+        player1.myTurn = player2.roll();
+      };
+      // player1.roll();
+      console.log("player1 ", player1);
+      console.log("player2 ", player2);
     });
+
+    $("#buttonPass").click(function() {
+      if (player1.myTurn) {
+        player2.myTurn = player1.pass();
+      } else if (player2.myTurn) {
+        player1.myTurn = player2.pass();
+      };
+      console.log("player1 ", player1);
+      console.log("player2 ", player2);
+    })
   });
 });
