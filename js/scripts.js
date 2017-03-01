16d7194231f3710449c951374367e1c0ec05b5f6
Janek Brandt and Liam Stabeno <liam@>
2017-03-01 10:54:44 -0800

    pass roll variable to scoreboard function and show the current roll to the user

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ function Player(turn) {
   this.win = false;
 };
 
-Player.prototype.roll = function() {
+Player.prototype.roll = function(roll) {
   if (this.myTurn) {
-    var roll = Math.ceil(Math.random() * 6);
+
     // var roll = 50;
     if (roll > 1) {
       this.tempScore += roll;
@@ -41,17 +41,17 @@ $(function() {
   var player1 = new Player(true);
   var player2 = new Player(false);
 
-  function updateScoreBoard() {
+  function updateScoreBoard(roll) {
     $("#player1Score").text(player1.score);
-    $("#player1TempScore").text(player1.tempScore);
     $("#player2Score").text(player2.score);
-    $("#player2TempScore").text(player2.tempScore);
+    $("#rollTotal").text(player1.tempScore+player2.tempScore);
+    $("#roll").text(roll);
   };
 
   function newGame() {
     player1.reset(true);
     player2.reset(false);
-    updateScoreBoard();
+    updateScoreBoard(0);
   };
 
   $("#buttonStart").click(function() {
@@ -59,20 +59,21 @@ $(function() {
   });
 
   $("#buttonRoll").click(function() {
+    var roll = Math.ceil(Math.random() * 6);
     if (player1.myTurn) {
-      player2.myTurn = player1.roll();
+      player2.myTurn = player1.roll(roll);
       if (player1.win) {
         alert("player1 Won")
         newGame();
       }
     } else if (player2.myTurn) {
-      player1.myTurn = player2.roll();
+      player1.myTurn = player2.roll(roll);
       if (player2.win) {
         alert("player2 Won")
         newGame();
       }
     };
-    updateScoreBoard();
+    updateScoreBoard(roll);
   });
 
   $("#buttonPass").click(function() {
@@ -81,6 +82,6 @@ $(function() {
     } else if (player2.myTurn) {
       player1.myTurn = player2.pass();
     };
-    updateScoreBoard();
+    updateScoreBoard(0);
   });
 });
