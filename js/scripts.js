bb913e91b5171d393c51ebb47002a2a536051678
Janek Brandt and Liam Stabeno <liam@>
2017-03-01 09:27:31 -0800

    add method to player object to roll random number between 1-6

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,25 @@ function Player() {
   this.tempScore = 0;
 };
 
+Player.prototype.roll = function() {
+  var roll = Math.ceil(Math.random() * 6);
+  console.log(roll);
+  if (roll > 1) {
+    this.tempScore += roll;
+  } else {
+    // rolled 1
+  };
+};
+
 // Front-End
 $(function() {
   $("#buttonStart").click(function() {
     var player1 = new Player();
     console.log(player1);
+
+    $("#buttonRoll").click(function() {
+      player1.roll();
+      console.log(player1);
+    });
   });
 });
