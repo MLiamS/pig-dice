2167b71118180e6c63454756d5e7094eb9ec2885
Janek Brandt and Liam Stabeno <liam@>
2017-03-01 10:35:09 -0800

    add win variable to player object, this gets set to true when player score is 100. alert gets shown when playerwon and game is reset

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,18 @@ function Player(turn) {
   this.score = 0;
   this.tempScore = 0;
   this.myTurn = turn;
+  this.win = false;
 };
 
 Player.prototype.roll = function() {
   if (this.myTurn) {
     var roll = Math.ceil(Math.random() * 6);
-    console.log(roll);
+    // var roll = 50;
     if (roll > 1) {
       this.tempScore += roll;
+      if (this.score + this.tempScore >= 100) {
+        this.win = true;
+      }
     } else {
       this.tempScore = 0
       return this.pass();
@@ -19,40 +23,64 @@ Player.prototype.roll = function() {
 };
 
 Player.prototype.pass = function() {
-  console.log("pass called");
   this.score += this.tempScore;
   this.tempScore = 0;
   this.myTurn = false;
   return true;
 };
 
+Player.prototype.reset = function(turn) {
+  this.score = 0;
+  this.tempScore = 0;
+  this.myTurn = turn;
+  this.win = false;
+}
+
 // Front-End
 $(function() {
+  var player1 = new Player(true);
+  var player2 = new Player(false);
+
+  function updateScoreBoard() {
+    $("#player1Score").text(player1.score);
+    $("#player1TempScore").text(player1.tempScore);
+    $("#player2Score").text(player2.score);
+    $("#player2TempScore").text(player2.tempScore);
+  };
+
+  function newGame() {
+    player1.reset(true);
+    player2.reset(false);
+    updateScoreBoard();
+  };
+
   $("#buttonStart").click(function() {
-    var player1 = new Player(true);
-    var player2 = new Player(false);
-    console.log("player1 ", player1);
-    console.log("player2 ", player2);
+    newGame();
+  });
 
-    $("#buttonRoll").click(function() {
-      if (player1.myTurn) {
-        player2.myTurn = player1.roll();
-      } else if (player2.myTurn) {
-        player1.myTurn = player2.roll();
-      };
-      // player1.roll();
-      console.log("player1 ", player1);
-      console.log("player2 ", player2);
-    });
+  $("#buttonRoll").click(function() {
+    if (player1.myTurn) {
+      player2.myTurn = player1.roll();
+      if (player1.win) {
+        alert("player1 Won")
+        newGame();
+      }
+    } else if (player2.myTurn) {
+      player1.myTurn = player2.roll();
+      if (player2.win) {
+        alert("player2 Won")
+        newGame();
+      }
+    };
+    updateScoreBoard();
+  });
 
-    $("#buttonPass").click(function() {
-      if (player1.myTurn) {
-        player2.myTurn = player1.pass();
-      } else if (player2.myTurn) {
-        player1.myTurn = player2.pass();
-      };
-      console.log("player1 ", player1);
-      console.log("player2 ", player2);
-    })
+  $("#buttonPass").click(function() {
+    if (player1.myTurn) {
+      player2.myTurn = player1.pass();
+    } else if (player2.myTurn) {
+      player1.myTurn = player2.pass();
+    };
+    updateScoreBoard();
   });
 });
